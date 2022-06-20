d23188f57c76e98a0271e499f1b79c15075b4597
LizzieDworsky <amydworsky15@gmail.com>
2022-06-20 13:19:38 -0600

    added axios to dependencies + connected backend to frontend using states and async

--- music-library/src/App.jsx
+++ music-library/src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-    return <div>Hello World</div>;
+    const [songs, setSongs] = useState([]);
+
+    useEffect(() => {
+        getAllSongs();
+    }, []);
+
+    async function getAllSongs() {
+        let response = await axios.get("http://127.0.0.1:8000/song/");
+        setSongs(response.data);
+    }
+
+    return (
+        <div>
+            <footer>
+                <button onClick={getAllSongs}>Remake API Call</button>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
